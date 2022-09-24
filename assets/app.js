f66a6b5b71f49a23961d4a25cece2f77b901cc55
FraneSpralja <spralja.frane@gmail.com>
2022-09-24 18:03:12 -0300

    add validation to formulario

--- assets/app.js
+++ assets/app.js
@@ -13,21 +13,48 @@ const regexLetter = /[a-z]/gi
 eventListener()
 
 function eventListener() {
+    
     formulario.addEventListener('submit', (e) => {
         e.preventDefault()
-
+        
         formValidation()
+        
+        nameInput.addEventListener('focus', validationName(regexLetter, nameInput));
+        numberInput.addEventListener('focus', validationNumber(regexNum, numberInput));
+        monthInput.addEventListener('focus', validationNumber(regexNum, monthInput));
+        yearInput.addEventListener('focus', validationNumber(regexNum, yearInput));
+        cvcInput.addEventListener('focus', validationNumber(regexNum, cvcInput));
 
-        nameInput.addEventListener('input', validationName(regexLetter, nameInput));
-        numberInput.addEventListener('input', validationNumber(regexNum, numberInput));
-        monthInput.addEventListener('input', validationNumber(regexNum, monthInput));
-        yearInput.addEventListener('input', validationNumber(regexNum, yearInput));
-        cvcInput.addEventListener('input', validationNumber(regexNum, cvcInput));
 
     })
 }
 
 // FUNCTION
+function validationNumber(regex, input) {
+        if(input.value !== ''){
+            const cardNumberValidation = regex.test(input.value)
+        
+            if(cardNumberValidation){
+                if(input.classList.contains('val-error')) input.classList.remove('val-error')
+            }else{
+                input.classList.add('val-error')
+                printMesagge('Wrong format, number only', input, 'text')
+            }
+        }
+}
+
+function validationName(regex, input){
+    if(input.value !== ''){
+        const nameValidation = regex.test(input.value)
+        
+            if(nameValidation){
+                if(input.classList.contains('val-error')) input.classList.remove('val-error')
+            }else{
+                input.classList.add('val-error')
+                printMesagge("Name can't have numbers", input, 'number')
+            }
+    }
+}
 
 function formValidation() {
     const allInput = document.querySelectorAll('#form input')
@@ -36,33 +63,33 @@ function formValidation() {
         const input = allInput[i]
         if(input.value == '') {
             input.classList.add('val-error');
-            printMesagge("Can't be blank")
+            printMesagge("Can't be blank", input, 'onblank')
         }
     }
 }
 
-function validationNumber(regex, input) {
-        const cardNumberValidation = regex.test(input.value)
-    
-        if(cardNumberValidation){
-            if(input.classList.contains('val-error')) input.classList.remove('val-error')
-        }else{
-            input.classList.add('val-error')
-            printMesagge('Wrong format, number only')
+
+function printMesagge(msg, input, type) {
+    const formBox = document.querySelectorAll('.form_box')
+
+    for(let i = 0; i < formBox.length; i++){
+
+        if(formBox[i].contains(input)){
+            
+            const divMessage = document.createElement('div');
+            divMessage.classList.add('bloqueMensaje');
+        
+            const paragraph = document.createElement('p');
+            paragraph.classList.add('message', type)
+            paragraph.textContent = msg;
+        
+            divMessage.appendChild(paragraph)
+        
+            formBox[i].appendChild(divMessage)
+
+            setTimeout(() => {
+                divMessage.remove()
+            }, 3500)
         }
-}
-
-function validationName(regex, input){
-    const nameValidation = regex.test(input.value)
-
-    if(nameValidation){
-        if(input.classList.contains('val-error')) input.classList.remove('val-error')
-    }else{
-        input.classList.add('val-error')
-        printMesagge("Name can't have numbers")
     }
-}
-
-function printMesagge(msg) {
-    console.log(msg)
 }
